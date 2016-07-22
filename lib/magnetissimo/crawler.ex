7f797aaaaefcfc1ebabb6d6d6954fdfde702867d
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-07-22 18:36:25 -0400

    Initial working version of crawler.

--- lib/magnetissimo/crawler.ex
+++ lib/magnetissimo/crawler.ex
@@ -1,35 +1,32 @@
 defmodule Magnetissimo.Crawler do
   def crawl(url, root, previously_crawled) do
     cond do
-      String.starts_with?(url, "magnet:") ->
+      String.starts_with?(url, "magnet") ->
         create_torrent(url)
       true ->
         cond do
           url in previously_crawled ->
-            IO.puts "Already crawled, in collection: #{previously_crawled}"
-            {:no_work, previously_crawled}
+            IO.puts "Duplicate url: #{url}"
           true ->
-            IO.puts "Processing #{url}"
+            IO.puts "Crawling: #{url}"
+            Process.sleep(1000)
             case HTTPoison.get(url) do
               {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
                 body
                 |> Floki.find("a")
                 |> Floki.attribute("href")
                 |> Enum.each(fn(href) ->
-                  IO.puts "--Crawl next #{href}"
                   cond do
                     String.starts_with?(href, "/")  ->
                       crawl_url = root <> href
-                      IO.puts "----Diving deep #{crawl_url}"
-                      crawl(crawl_url, root, [crawl_url | previously_crawled])
+                      crawl(crawl_url, root, [url | previously_crawled])
                     true ->
-                      IO.puts "----External link, ignoring"
                   end
                 end)
               {:ok, %HTTPoison.Response{status_code: 404}} ->
                 IO.puts "Not found :("
-              {:error, %HTTPoison.Error{reason: reason}} ->
-                IO.inspect reason
+              _ ->
+                IO.puts "Who knows lol"
             end
         end
     end
