c24182d35b1d30e7597c0884452cfabffb39649e
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-08-21 06:49:45 -0400

    Don't queue things non-stop, wait until queue is empty.

--- lib/crawler.ex
+++ lib/crawler.ex
@@ -13,49 +13,59 @@ defmodule Magnetissimo.Crawler do
   end
 
   def handle_info(:work, state) do
-    Logger.info "Initiating crawler run."
+    {:ok, queues} = Exq.Api.queue_size(Exq.Api.Server.server_name(nil))
 
-    Magnetissimo.Parsers.ThePirateBay.root_urls
-    |> Enum.each(fn url -> 
-      Logger.info "Queued: ThePirateBay root_urls"
-      Exq.enqueue(Exq, "thepiratebay", "Magnetissimo.DownloadWorker", [url, "thepiratebay", "root_url"])
-    end)
+    queue_sizes = for {_q, size} <- queues do
+      size
+    end
+    enqueued_jobs = Enum.sum(queue_sizes)
 
-    Magnetissimo.Parsers.Demonoid.root_urls
-    |> Enum.each(fn url -> 
-      Logger.info "Queued: Demonoid root_urls"
-      Exq.enqueue(Exq, "demonoid", "Magnetissimo.DownloadWorker", [url, "demonoid", "root_url"])
-    end)
+    if enqueued_jobs == 0 do
+      Logger.info "Enqueued jobs exist, still running."
+    else
+      Logger.info "Initiating crawler run."
+      Magnetissimo.Parsers.ThePirateBay.root_urls
+      |> Enum.each(fn url -> 
+        Logger.info "Queued: ThePirateBay root_urls"
+        Exq.enqueue(Exq, "thepiratebay", "Magnetissimo.DownloadWorker", [url, "thepiratebay", "root_url"])
+      end)
 
-    Magnetissimo.Parsers.Isohunt.root_urls
-    |> Enum.each(fn url -> 
-      Logger.info "Queued: Isohunt root_urls"
-      Exq.enqueue(Exq, "isohunt", "Magnetissimo.DownloadWorker", [url, "isohunt", "root_url"])
-    end)
+      Magnetissimo.Parsers.Demonoid.root_urls
+      |> Enum.each(fn url -> 
+        Logger.info "Queued: Demonoid root_urls"
+        Exq.enqueue(Exq, "demonoid", "Magnetissimo.DownloadWorker", [url, "demonoid", "root_url"])
+      end)
 
-    Magnetissimo.Parsers.Limetorrents.root_urls
-    |> Enum.each(fn url -> 
-      Logger.info "Queued: Limetorrents root_urls"
-      Exq.enqueue(Exq, "limetorrents", "Magnetissimo.DownloadWorker", [url, "limetorrents", "root_url"])
-    end)
+      Magnetissimo.Parsers.Isohunt.root_urls
+      |> Enum.each(fn url -> 
+        Logger.info "Queued: Isohunt root_urls"
+        Exq.enqueue(Exq, "isohunt", "Magnetissimo.DownloadWorker", [url, "isohunt", "root_url"])
+      end)
 
-    Magnetissimo.Parsers.TorrentDownloads.root_urls
-    |> Enum.each(fn url -> 
-      Logger.info "Queued: TorrentDownloads root_urls"
-      Exq.enqueue(Exq, "torrentdownloads", "Magnetissimo.DownloadWorker", [url, "torrentdownloads", "root_url"])
-    end)
+      Magnetissimo.Parsers.Limetorrents.root_urls
+      |> Enum.each(fn url -> 
+        Logger.info "Queued: Limetorrents root_urls"
+        Exq.enqueue(Exq, "limetorrents", "Magnetissimo.DownloadWorker", [url, "limetorrents", "root_url"])
+      end)
 
-    Magnetissimo.Parsers.Leetx.root_urls
-    |> Enum.each(fn url -> 
-      Logger.info "Queued: 1337x root_urls"
-      Exq.enqueue(Exq, "leetx", "Magnetissimo.DownloadWorker", [url, "leetx", "root_url"])
-    end)
+      Magnetissimo.Parsers.TorrentDownloads.root_urls
+      |> Enum.each(fn url -> 
+        Logger.info "Queued: TorrentDownloads root_urls"
+        Exq.enqueue(Exq, "torrentdownloads", "Magnetissimo.DownloadWorker", [url, "torrentdownloads", "root_url"])
+      end)
+
+      Magnetissimo.Parsers.Leetx.root_urls
+      |> Enum.each(fn url -> 
+        Logger.info "Queued: 1337x root_urls"
+        Exq.enqueue(Exq, "leetx", "Magnetissimo.DownloadWorker", [url, "leetx", "root_url"])
+      end)
+    end
 
     schedule_work()
     {:noreply, state}
   end
 
   defp schedule_work() do
-    Process.send_after(self(), :work, 3_600_000) # 1 hour
+    Process.send_after(self(), :work, 300) # 1 hour
   end
 end
