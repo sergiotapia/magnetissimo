8a501187ccd107d60e30d4f77c0b00bb2df91ed3
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2017-01-28 00:49:21 -0500

    Working search and pagination.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -2,6 +2,19 @@ defmodule Magnetissimo.PageView do
   use Magnetissimo.Web, :view
   import Scrivener.HTML
 
+  def readable_size(bytes) do
+    cond do
+      # bytes < 1024 ->
+      #   "#{bytes} B"
+      # bytes < 1048576 ->
+      #   "#{bytes/1024} KB"
+      # bytes < 1073741824 ->
+      #   "#{bytes/1048576} MB"
+      true ->
+        "#{bytes}"
+    end
+  end
+
   def seeders(seeders) do
     seeders || 0
   end
