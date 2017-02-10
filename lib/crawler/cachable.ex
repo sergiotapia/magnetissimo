260ffe9592a0a42066e4baf1ee7ada95ee730b3d
Christian Fei <crifei93@gmail.com>
2017-02-10 23:48:14 +0100

    add cachable crawler

--- lib/crawler/cachable.ex
+++ lib/crawler/cachable.ex
@@ -1,11 +1,13 @@
-defmodule Magnetissimo.Crawler.Common do
+defmodule Magnetissimo.Crawler.Cachable do
   defmacro __using__(_) do
     quote do
 			alias Magnetissimo.KV
 			def process({:torrent_link_cache, url}, queue) do
 				case KV.cached?(url) do
-          {:ok, timestamp} -> process({:torrent_link, url}, queue)
-          false -> IO.puts "Skipping #{url}"
+          {:ok, timestamp} -> IO.puts "Already cached at #{timestamp} #{url}"
+          false ->
+            queue = [{:torrent_link, url} | queue]
+            KV.cache(url)
         end
 				queue
 			end
---
+++ test/lib/crawler/cachable_test.exs
@@ -0,0 +1,55 @@
+defmodule Magnetissimo.Crawler.CachableExample do
+  use GenServer
+  use Magnetissimo.Crawler.Cachable
+
+  def start_link do
+    GenServer.start_link(__MODULE__, initial_queue)
+  end
+
+  def init(queue) do
+    GenServer.cast(self, :work)
+    Process.send_after(self(), :work, 1 * 1 * 100) # 5 seconds
+    {:ok, queue}
+  end
+
+  def initial_queue do
+    :queue.from_list(["some.url"])
+  end
+
+  def process({:torrent_link, url}, queue) do
+    Enum.filter(queue, fn(item) -> item !== {:torrent_link, url} end)
+  end
+  def process({:page_link, url}, queue) do
+  end
+
+  def process(_anything, queue) do
+    queue
+  end
+
+  # Callbacks
+
+  def handle_cast(:work, queue) do
+    case :queue.out(queue) do
+      {{_value, item}, queue_2} ->
+        queue = queue_2
+        queue = process(item, queue)
+      _ ->
+        IO.puts "Queue is empty - restarting queue."
+        queue = initial_queue
+    end
+    {:noreply, queue}
+  end
+end
+
+defmodule Magnetissimo.Crawler.CachableTest do
+  use ExUnit.Case, async: true
+
+  test "keeps track of cached torrent_links" do
+    {:ok, _} = Magnetissimo.KV.start_link
+    {:ok, _} = Magnetissimo.Crawler.CachableExample.start_link
+
+    assert [{:torrent_link, "some.url"}] == Magnetissimo.Crawler.CachableExample.process({:torrent_link_cache, "some.url"}, [])
+    assert [] == Magnetissimo.Crawler.CachableExample.process({:torrent_link_cache, "some.url"}, [])
+  end
+
+end
