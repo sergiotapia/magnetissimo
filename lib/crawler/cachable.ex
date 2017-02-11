49b0fbac629042adc32ccbe27527f638bd33f4ec
Christian Fei <crifei93@gmail.com>
2017-02-11 07:58:08 +0100

    add simple expiry mechanism

--- lib/crawler/cachable.ex
+++ lib/crawler/cachable.ex
@@ -4,14 +4,24 @@ defmodule Magnetissimo.Crawler.Cachable do
 			alias Magnetissimo.KV
 			def process({:torrent_link_cache, url}, queue) do
 				case KV.cached?(url) do
-          false ->
-            process({:torrent_link, url}, queue)
-            KV.cache(url)
-            IO.puts "Caching #{url}"
-          {:ok, timestamp} -> IO.puts "Already cached at #{timestamp} #{url}"
+          false -> process_cache(url, queue)
+          {:ok, timestamp} ->
+            now = :os.system_time(:seconds)
+            if timestamp < now - 1*60*60 do
+              IO.puts "Cache expired for #{url}"
+              process_cache(url, queue)
+            else
+              IO.puts "Already cached at #{timestamp} #{url}"
+            end
         end
 				queue
 			end
+
+      def process_cache(url, queue) do
+        process({:torrent_link, url}, queue)
+        KV.cache(url)
+        IO.puts "Caching #{url}"
+      end
     end
   end
 end
