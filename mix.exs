d079ccf031f4aebdd8234ee4abc98537258651bd
skwerlman <skwerlman@gmail.com>
2017-08-01 06:23:24 -0400

    remove errant changes from previous commits

--- mix.exs
+++ mix.exs
@@ -33,20 +33,19 @@ defmodule Magnetissimo.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
+     {:postgrex, ">= 0.13.2"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:credo, "~> 0.7", only: [:dev, :test]},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:floki, "~> 0.17.2"},
-     {:httpoison, "~> 0.12"},
+     {:httpoison, "~> 0.10.0"},
      {:html_entities, "~> 0.3"},
      {:distillery, "~> 1.4"},
      {:scrivener_ecto, "~> 1.0"},
      {:scrivener_html, "~> 1.1"},
-     {:sizeable, "~> 0.1.5"},
-     {:credo, "~> 0.8", only: [:dev, :test], runtime: false}]
+     {:sizeable, "~> 0.1.5"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
