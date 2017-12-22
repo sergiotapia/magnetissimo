f6a7a74f891e6e710b5bbf9895d2e02e1a6ad4fb
Théophile Choutri <theophile@choutri.eu>
2017-12-23 00:52:55 +0100

    Bump the versions

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Magnetissimo.Mixfile do
   def project do
     [
       app: :magnetissimo,
-      version: "0.0.1",
-      elixir: "~> 1.4",
+      version: "0.1.0",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
