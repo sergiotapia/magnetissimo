0145df5e0d4fecdf409da068ae0c75918c4099d5
Théophile Choutri <theophile@choutri.eu>
2017-12-24 09:47:15 +0100

    enhance the structure of the code.

--- lib/magnetissimo/crawler/demonoid.ex
+++ lib/magnetissimo/crawler/demonoid.ex
@@ -11,7 +11,7 @@ defmodule Magnetissimo.Crawler.Demonoid do
 
   def initial_queue do
     urls = for i <- 1..5 do
-      {:page_link, "https://www.demonoid.pw/rss/0.xml"}
+      {:page_link, "https://www.demonoid.pw/files/?to=0&uid=0&category=0&subcategory=0&language=0&seeded=2&quality=0&external=2&query=&sort=&page=#{i}"}
     end
     :queue.from_list(urls)
   end
@@ -22,13 +22,21 @@ defmodule Magnetissimo.Crawler.Demonoid do
   end
 
   def init(queue) do
-    schedule_work()
+    Logger.info IO.ANSI.magenta <> "[Demonoid] Starting the crawler" <> IO.ANSI.reset
+    schedule_start()
     {:ok, queue}
   end
 
-  defp schedule_work do
-    wait_seconds = :rand.uniform(8) * 1000
-    Process.send_after(self(), :work, wait_seconds)
+  def schedule_start() do
+    wait = :rand.uniform(8) * 1000 # [1,8] seconds
+    Logger.debug "[Demonoid] Starting work in #{wait / 1000}s…"
+    Process.send_after(self(), :work, wait)
+  end
+
+  def schedule_work() do
+    wait = 1800000 # 30mn
+    Logger.info("[Demonoid] Finished crawling the RSS feed, waiting 30 minutes…")
+    Process.send_after(self(), :work, wait)
   end
 
   def handle_info(:work, queue) do
@@ -40,53 +48,59 @@ defmodule Magnetissimo.Crawler.Demonoid do
         Logger.info "[Demonoid] Queue is empty, restarting scraping procedure."
         initial_queue()
     end
-    schedule_work()
+    Logger.debug("[Demonoid] New queue: " <> inspect new_queue)
+    schedule_start()
     {:noreply, new_queue}
   end
 
   def process({:page_link, url}, queue) do
     Logger.info "[Demonoid] Finding torrents in listing page: #{url}"
 
-    result = download(url) |> torrent_links
-
-    case result do
+    with {:ok, body}     <- download(url),
+         {:ok, torrents} <- torrent_links(body) do
+            Logger.debug "[Demonoid] Putting torrents in the queue"
+            Enum.reduce(torrents, queue, fn torrent, queue ->
+              :queue.in({:torrent_link, torrent}, queue)
+            end)
+    else
       {:error, message} ->
         Logger.error message
         queue
-      torrents ->
-        Enum.reduce(torrents, queue, fn torrent, queue ->
-          :queue.in({:torrent_link, torrent}, queue)
-        end)
     end
   end
 
   def process({:torrent_link, url}, queue) do
-    result = download(url) |> torrent_information 
+    Logger.debug "[Demonoid] Processing #{url}"
+    with {:ok, body}    <- download(url),
+         {:ok, torrent} <- torrent_information(body) do
 
-    case result do
-      {:error, message} -> 
-        Logger.error message
-      torrent_struct -> 
-        torrent_struct = Map.put(torrent_struct, :outbound_url, url)
-        Magnetissimo.Torrent.save_torrent(torrent_struct)
+           torrent
+           |> Map.put(:outbound_url, url)
+           |> Magnetissimo.Torrent.save_torrent
+    else
+      {:error, message} ->
+        Logger.error "[Demonoid] " <> message
     end
     queue
   end
 
   def torrent_links(html_body) when is_binary(html_body) and byte_size(html_body) > 50 do
-    html_body
+    Logger.debug "[Demonoid] Getting torrents"
+    result = html_body
     |> Floki.find("td.tone_1_pad a")
     |> Floki.attribute("href")
     |> Enum.filter(fn(a) -> String.contains?(a, "/files/details/") end)
     |> Enum.map(fn(url) -> "https://www.demonoid.pw" <> url end)
+    {:ok, result}
   end
 
   def torrent_links(_html_body) do
+    Logger.debug "meh :/"
     {:error, "Couldn't parse torrents links."}
   end
   
   def torrent_information(html_body) when is_binary(html_body) and byte_size(html_body) > 50 do
-
+    Logger.debug "Assembling informations"
     name = html_body
       |> Floki.find("td.ctable_header")
       |> Floki.text
@@ -116,16 +130,16 @@ defmodule Magnetissimo.Crawler.Demonoid do
 
     size_value = Enum.at(size, 0)
     unit = Enum.at(size, 1)
-    size = size_to_bytes(size_value, unit) |> Kernel.to_string
+    size = size_to_bytes(size_value, unit) |> Sizeable.filesize
 
-    %{
+    {:ok, %{
       name: name,
       magnet: magnet,
       size: size,
       website_source: "demonoid",
       seeders: 0,
       leechers: 0
-    }
+    }}
   end
 
   def torrent_information(_html_body) do
