6f8a2a57d718094471fd3c0690eac6adf51594cb
Théophile Choutri <theophile@choutri.eu>
2018-01-10 00:38:57 +0100

    Enable assets reloading with brunch and inotify

--- config/dev.exs
+++ config/dev.exs
@@ -10,7 +10,9 @@ config :magnetissimo, MagnetissimoWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false
+  check_origin: false,
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
+                    cd: Path.expand("../assets", __DIR__)]]
 
 # Watch static and templates for browser reloading.
 config :magnetissimo, MagnetissimoWeb.Endpoint,
