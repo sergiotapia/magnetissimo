ab24e2ab7249b9452be6ca1129b3e8bd2e7a240f
skwerlman <skwerlman@gmail.com>
2018-02-05 11:31:21 -0500

    actually filter out nsfw torrents

--- lib/magnetissimo_web/controllers/page_controller.ex
+++ lib/magnetissimo_web/controllers/page_controller.ex
@@ -6,15 +6,27 @@ defmodule MagnetissimoWeb.PageController do
 
   def index(conn, params) do
     results = case params do
-      %{"term" => term, "website_source" => website_source}
+      %{"term" => term, "nsfw" => "on", "website_source" => website_source}
         when website_source != "" ->
           from t in Torrent,
           where: ilike(t.name, ^"%#{term}%"),
           where: t.website_source == ^website_source,
           order_by: [desc: :inserted_at]
+      %{"term" => term, "website_source" => website_source}
+        when website_source != "" ->
+          from t in Torrent,
+          where: ilike(t.name, ^"%#{term}%"),
+          where: t.website_source == ^website_source,
+          where: t.nsfw == false,
+          order_by: [desc: :inserted_at]
+      %{"term" => term, "nsfw" => "on"} ->
+          from t in Torrent,
+          where: ilike(t.name, ^"%#{term}%"),
+          order_by: [desc: :inserted_at]
       _ ->
           from t in Torrent,
           where: ilike(t.name, ^"%#{params["term"]}%"),
+          where: t.nsfw == false,
           order_by: [desc: :inserted_at]
     end
 
