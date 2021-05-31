0a0832c04e2744241f949e874bf275cefbca0c36
Sergio Tapia <yeyo@hey.com>
2021-05-31 00:18:21 -0400

    Change backend type from string to text

---
+++ .iex.exs
@@ -0,0 +1,4 @@
+alias Magnetissimo.Repo
+alias Magnetissimo.Torrents.Torrent
+
+alias Magnetissimo.Crawlers.Nyaa
--- apps/magnetissimo/priv/repo/migrations/20210527164952_create_torrents.exs
+++ apps/magnetissimo/priv/repo/migrations/20210527164952_create_torrents.exs
@@ -4,12 +4,12 @@ defmodule Magnetissimo.Repo.Migrations.CreateTorrents do
   def change do
     create table(:torrents, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :name, :string
-      add :magnet_url, :string
-      add :canonical_url, :string
-      add :leechers, :string
-      add :seeders, :string
-      add :size_in_bytes, :string
+      add :name, :text
+      add :magnet_url, :text
+      add :canonical_url, :text
+      add :leechers, :text
+      add :seeders, :text
+      add :size_in_bytes, :text
       add :published_at, :naive_datetime
 
       timestamps()
