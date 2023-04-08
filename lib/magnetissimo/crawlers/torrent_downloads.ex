0f0d73ad26b56795078fdbb5fcf3a50bd32e38d2
Sergio Tapia <yeyo@hey.com>
2023-04-08 13:21:55 -0400

    TorrentDownloads torrent page crawling

--- lib/magnetissimo/crawlers/torrent_downloads.ex
+++ lib/magnetissimo/crawlers/torrent_downloads.ex
@@ -3,6 +3,103 @@ defmodule Magnetissimo.Crawlers.TorrentDownloads do
   require Logger
 
   alias Magnetissimo.Torrents
+  alias Magnetissimo.Utils
+
+  def search(search_term) do
+    source = Torrents.get_source_by_name!("TorrentDownloads")
+
+    1..1
+    |> Enum.each(fn page ->
+      search_term
+      |> get_search_page_html(page)
+      |> parse_search_page(source)
+    end)
+  end
+
+  def parse_search_page(search_page_html, source) do
+    search_page_html
+    |> Floki.parse_document!()
+    |> Floki.find(".grey_bar3 p a")
+    |> Enum.filter(fn link ->
+      Floki.attribute(link, "href") |> List.first() |> String.starts_with?("/torrent/")
+    end)
+    |> Floki.attribute("href")
+    |> Enum.map(fn href -> "https://www.torrentdownloads.pro#{href}" end)
+    |> Enum.each(fn torrent_url ->
+      torrent_url
+      |> get_torrent_page_html()
+      |> parse_torrent_page(torrent_url, source)
+    end)
+  end
+
+  def parse_torrent_page(torrent_page_html, canonical_url, source) do
+    torrent_page_html =
+      torrent_page_html
+      |> Floki.parse_document!()
+
+    name = torrent_page_html |> Floki.find("h1.titl_1") |> Floki.text()
+
+    leechers =
+      torrent_page_html
+      |> Floki.find(".grey_bar1, .grey_bara1")
+      |> Enum.filter(fn item ->
+        item |> Floki.text() |> String.contains?("Leechers")
+      end)
+      |> List.first()
+      |> Floki.text()
+      |> String.split(": ")
+      |> List.last()
+      |> Integer.parse()
+      |> elem(0)
+
+    seeders =
+      torrent_page_html
+      |> Floki.find(".grey_bar1, .grey_bara1")
+      |> Enum.filter(fn item ->
+        item |> Floki.text() |> String.contains?("Seeds")
+      end)
+      |> List.first()
+      |> Floki.text()
+      |> String.split(": ")
+      |> List.last()
+      |> Integer.parse()
+      |> elem(0)
+
+    magnet_url =
+      torrent_page_html
+      |> Floki.find("a[href^='magnet:?']")
+      |> Floki.attribute("href")
+      |> List.first()
+
+    size_in_bytes =
+      torrent_page_html
+      |> Floki.find(".grey_bar1, .grey_bara1")
+      |> Enum.filter(fn item ->
+        item |> Floki.text() |> String.contains?("Total Size")
+      end)
+      |> List.first()
+      |> Floki.text()
+      |> String.split(": ")
+      |> List.last()
+      |> IO.inspect()
+      |> Utils.size_to_bytes()
+
+    %{
+      name: name,
+      canonical_url: canonical_url,
+      leechers: leechers,
+      seeders: seeders,
+      magnet_url: magnet_url,
+      source_id: source.id,
+      size_in_bytes: size_in_bytes
+    }
+    |> IO.inspect()
+
+    # torrent = %{
+    #   published_at: torrent.published_at |> List.to_string() |> parse_published_at(),
+    #   category_id: category.id
+    # }
+  end
 
   def crawl_latest() do
     Logger.info("[TorrentDownloads] Crawling latest torrents.")
@@ -35,7 +132,8 @@ defmodule Magnetissimo.Crawlers.TorrentDownloads do
         "magnet:?xt=urn:btih:#{magnet_hash}&dn=#{magnet_name}&tr=udp%3A%2F%2Ftracker.open-internet.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.si%3A1337%2Fannounce&tr=udp%3A%2F%2Fdenis.stalker.upeer.me%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.port443.xyz%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2770%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2740%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2720%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2770%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2740%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337%2Fannounce"
 
       torrent = %{
-        canonical_url: "https://torrentdownloads.pro#{torrent.canonical_url |> List.to_string()}",
+        canonical_url:
+          "https://www.torrentdownloads.pro#{torrent.canonical_url |> List.to_string()}",
         leechers: torrent.leechers |> List.to_string() |> Integer.parse() |> elem(0),
         magnet_url: magnet_url,
         name: torrent.name |> List.to_string(),
@@ -64,6 +162,37 @@ defmodule Magnetissimo.Crawlers.TorrentDownloads do
   def parse_published_at(published_at) do
     {:ok, parsed_datetime} = Timex.parse(published_at, "{RFC1123}")
     Timex.to_datetime(parsed_datetime, "Etc/UTC")
+  end
+
+  @spec get_search_page_html(binary(), integer()) :: binary()
+  def get_search_page_html(search_term, page \\ 1) do
+    # Put some respek on they servers.
+    Process.sleep(350)
+    Logger.info("[TorrentDownloads] Fetching search results page.")
+
+    search_term =
+      search_term
+      |> String.replace(" ", "+")
+
+    %{status_code: 200, body: body} =
+      "https://www.torrentdownloads.pro/search/?page=#{page}&search=#{search_term}&s_cat=&srt=seeds&pp=50&order=desc"
+      |> HTTPoison.get!()
+
+    body
+  end
+
+  @spec get_torrent_page_html(binary()) :: binary()
+  def get_torrent_page_html(torrent_page_url) do
+    # Put some respek on they servers.
+    sleep_ms = 350 + :rand.uniform(200)
+    Process.sleep(sleep_ms)
+    Logger.info("[TorrentDownloads] Fetching torrent page: #{torrent_page_url}")
+
+    %{status_code: 200, body: body} =
+      torrent_page_url
+      |> HTTPoison.get!()
+
+    body
   end
 
   @spec get_rss :: binary()
