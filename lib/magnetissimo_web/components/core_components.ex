501de31eba451d9047f64ec18b86a0e04cf37dc4
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2023-04-10 14:07:23 -0400

    More styles getting there

--- lib/magnetissimo_web/components/core_components.ex
+++ lib/magnetissimo_web/components/core_components.ex
@@ -213,8 +213,7 @@ defmodule MagnetissimoWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        "button",
         @class
       ]}
       {@rest}
