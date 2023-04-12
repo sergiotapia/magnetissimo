26b143b472c176bef66ded10f03ad64e52db8eb8
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2023-04-12 16:26:08 -0400

    Set 2.0.0 version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Magnetissimo.MixProject do
   def project do
     [
       app: :magnetissimo,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
