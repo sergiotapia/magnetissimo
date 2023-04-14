7ed3b188b61be24e40f929c89423c28d773ce0d2
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2023-04-13 23:20:42 -0400

    Attempt to fix the localhost issue on prod deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :magnetissimo, MagnetissimoWeb.Endpoint,
