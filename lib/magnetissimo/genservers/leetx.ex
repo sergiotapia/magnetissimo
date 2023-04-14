9ddd6e50798eecddb0f09c4897aaa1bd543dce64
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2023-04-14 13:29:49 -0400

    Automatic latest crawlers

---
+++ lib/magnetissimo/genservers/leetx.ex
@@ -0,0 +1,25 @@
+defmodule Magnetissimo.Genservers.Leetx do
+  use GenServer
+  require Logger
+  alias Magnetissimo.Crawlers.Leetx
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(state) do
+    schedule_crawl_latest()
+    {:ok, state}
+  end
+
+  def handle_info(:crawl_latest, state) do
+    Leetx.crawl_latest()
+    schedule_crawl_latest()
+    {:noreply, state}
+  end
+
+  defp schedule_crawl_latest do
+    # Crawl latest every 5 minutes.
+    Process.send_after(self(), :crawl_latest, 300_000)
+  end
+end
