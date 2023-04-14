9542a9c148d2a68c76ac48aeb2dc7359db199f95
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2023-04-14 00:08:52 -0400

    Try to fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "localhost"
+  host = System.get_env("PHX_HOST") || "0.0.0.0"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :magnetissimo, MagnetissimoWeb.Endpoint,
