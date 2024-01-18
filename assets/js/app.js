3258db1c88bb008745bd09a755a1120921d29270
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2024-01-18 16:07:55 -0500

    Dark mode is now toggle button. #172

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,15 +18,15 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
@@ -38,4 +38,23 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+function darkExpected() {
+  return localStorage.theme === 'dark' || (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches);
+}
+
+function initDarkMode() {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (darkExpected()) document.documentElement.classList.add('dark');
+  else document.documentElement.classList.remove('dark');
+}
+
+window.addEventListener("toogle-darkmode", e => {
+  if (darkExpected()) localStorage.theme = 'light';
+  else localStorage.theme = 'dark';
+  initDarkMode();
+})
+
+initDarkMode();
 
